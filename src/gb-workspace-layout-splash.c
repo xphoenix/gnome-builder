25471c2c9c6a7bb461f3dfa3935497a977a4e76e
Christian Hergert <christian@hergert.me>
2013-08-01 09:57:53 +0200

    splash: port to GtkListBox.

--- src/gb-workspace-layout-splash.c
+++ src/gb-workspace-layout-splash.c
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <egg/egg-list-box.h>
 #include <gd/gd-header-bar.h>
 #include <glib/gi18n.h>
 
@@ -53,17 +52,23 @@ gb_workspace_layout_splash_class_init (GbWorkspaceLayoutSplashClass *klass)
 }
 
 static void
-gb_workspace_layout_splash_child_activated (EggListBox              *list_box,
-                                            GtkWidget               *row,
-                                            GbWorkspaceLayoutSplash *splash)
+gb_workspace_layout_splash_row_activated (GtkListBox              *list_box,
+                                          GtkWidget               *row,
+                                          GbWorkspaceLayoutSplash *splash)
 {
    GbWorkspace *workspace;
    const gchar *path;
    GbProject *project;
+   GtkWidget *child;
+
+   g_assert(GTK_IS_LIST_BOX(list_box));
+   g_assert(GTK_IS_LIST_BOX_ROW(row));
+   g_assert(GB_IS_WORKSPACE_LAYOUT_SPLASH(splash));
 
    workspace = GB_WORKSPACE(gtk_widget_get_toplevel(row));
+   child = gtk_bin_get_child(GTK_BIN(row));
 
-   if (!(path = g_object_get_data(G_OBJECT(row), "path"))) {
+   if (!(path = g_object_get_data(G_OBJECT(child), "path"))) {
       project = g_object_new(GB_TYPE_PROJECT,
                              "name", "Unnamed Project",
                              NULL);
@@ -76,19 +81,29 @@ gb_workspace_layout_splash_child_activated (EggListBox              *list_box,
 }
 
 static void
-update_separators (GtkWidget **separator,
-                   GtkWidget  *child,
-                   GtkWidget  *before,
-                   gpointer    user_data)
+update_separators (GtkListBoxRow *row,
+                   GtkListBoxRow *before,
+                   gpointer       user_data)
 {
+   GtkWidget *header;
+
+   g_assert(GTK_IS_LIST_BOX_ROW(row));
+   g_assert(!before || GTK_IS_LIST_BOX_ROW(before));
+   g_assert(!user_data);
+
+   header = gtk_list_box_row_get_header(row);
+
    if (!before) {
+      if (header) {
+         gtk_widget_destroy(header);
+      }
       return;
    }
 
-   if (!*separator) {
-      *separator = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);
-      gtk_widget_show(*separator);
-      g_object_ref_sink(*separator);
+   if (!header) {
+      header = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);
+      gtk_list_box_row_set_header(row, header);
+      gtk_widget_show(header);
    }
 }
 
@@ -215,16 +230,19 @@ gb_workspace_layout_splash_init (GbWorkspaceLayoutSplash *splash)
                            NULL);
    gtk_container_add(GTK_CONTAINER(frame_), viewport);
 
-   splash->priv->list_box = g_object_new(EGG_TYPE_LIST_BOX,
+   splash->priv->list_box = g_object_new(GTK_TYPE_LIST_BOX,
                                          "visible", TRUE,
                                          NULL);
-   egg_list_box_set_separator_funcs(EGG_LIST_BOX(splash->priv->list_box),
-                                    update_separators, NULL, NULL);
-   egg_list_box_set_selection_mode(EGG_LIST_BOX(splash->priv->list_box),
+   gtk_list_box_set_header_func(GTK_LIST_BOX(splash->priv->list_box),
+                                update_separators, NULL, NULL);
+   gtk_list_box_set_selection_mode(GTK_LIST_BOX(splash->priv->list_box),
                                    GTK_SELECTION_NONE);
+   gtk_list_box_set_activate_on_single_click(
+         GTK_LIST_BOX(splash->priv->list_box),
+         TRUE);
    g_signal_connect(splash->priv->list_box,
-                    "child-activated",
-                    G_CALLBACK(gb_workspace_layout_splash_child_activated),
+                    "row-activated",
+                    G_CALLBACK(gb_workspace_layout_splash_row_activated),
                     splash);
    gtk_container_add(GTK_CONTAINER(viewport), splash->priv->list_box);
 
