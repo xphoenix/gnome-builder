8bf3bc1a6267986f82af8e79a630a0bc6a34a85a
Christian Hergert <christian@hergert.me>
2013-09-13 03:58:35 -0700

    search: add fuzzy search test.

---
+++ tests/test-search-fuzzy.c
@@ -0,0 +1,37 @@
+#include "fuzzy.h"
+
+static void
+test_search_fuzzy (void)
+{
+   FuzzyMatch match;
+   GArray *matches;
+   Fuzzy *fuzzy;
+
+   fuzzy = fuzzy_new();
+
+   fuzzy_insert(fuzzy, "gtk_widget_show");
+   fuzzy_insert(fuzzy, "gtk_widget_hide");
+
+   matches = fuzzy_match(fuzzy, "gtkwtshw", 0);
+   g_assert_cmpint(matches->len, ==, 1);
+   match = g_array_index(matches, FuzzyMatch, 0);
+   g_assert_cmpstr(match.text, ==, "gtk_widget_show");
+   g_array_unref(matches);
+
+   matches = fuzzy_match(fuzzy, "kwthde", 0);
+   g_assert_cmpint(matches->len, ==, 1);
+   match = g_array_index(matches, FuzzyMatch, 0);
+   g_assert_cmpstr(match.text, ==, "gtk_widget_hide");
+   g_array_unref(matches);
+
+   fuzzy_free(fuzzy);
+}
+
+gint
+main (gint argc,
+      gchar *argv[])
+{
+   g_test_init(&argc, &argv, NULL);
+   g_test_add_func("/Search/Fuzzy", test_search_fuzzy);
+   return g_test_run();
+}
