b2a5d4db0deec65f2d9fad9e20aa2ab6d20492f7
Christian Hergert <christian@hergert.me>
2013-09-18 00:09:26 -0700

    tests: track fuzzy api.

--- tests/test-search-fuzzy.c
+++ tests/test-search-fuzzy.c
@@ -9,19 +9,19 @@ test_search_fuzzy (void)
 
    fuzzy = fuzzy_new();
 
-   fuzzy_insert(fuzzy, "gtk_widget_show");
-   fuzzy_insert(fuzzy, "gtk_widget_hide");
+   fuzzy_insert(fuzzy, "gtk_widget_show", NULL);
+   fuzzy_insert(fuzzy, "gtk_widget_hide", NULL);
 
    matches = fuzzy_match(fuzzy, "gtkwtshw", 0);
    g_assert_cmpint(matches->len, ==, 1);
    match = g_array_index(matches, FuzzyMatch, 0);
-   g_assert_cmpstr(match.text, ==, "gtk_widget_show");
+   g_assert_cmpstr(match.key, ==, "gtk_widget_show");
    g_array_unref(matches);
 
    matches = fuzzy_match(fuzzy, "kwthde", 0);
    g_assert_cmpint(matches->len, ==, 1);
    match = g_array_index(matches, FuzzyMatch, 0);
-   g_assert_cmpstr(match.text, ==, "gtk_widget_hide");
+   g_assert_cmpstr(match.key, ==, "gtk_widget_hide");
    g_array_unref(matches);
 
    fuzzy_free(fuzzy);
