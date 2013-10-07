24b4be3b4cc9da2211e3e7d95bbebf3e9ed531b0
Christian Hergert <christian@hergert.me>
2013-10-06 23:16:24 -0700

    tests: wire up project parsing and icons into browser.

--- tests/test-browser.c
+++ tests/test-browser.c
@@ -1,16 +1,26 @@
 #include "gb-application-resource.h"
+#include "gb-icon-theme.h"
 #include "gb-tree.h"
 #include "gb-tree-node.h"
+#include "gb-tree-node-project.h"
 #include "gb-tree-builder.h"
+#include "gb-tree-builder-project.h"
 #include "gb-project.h"
+#include "gb-project-file.h"
+#include "gb-project-group.h"
+#include "gb-project-item.h"
 #include "gb-project-format.h"
+#include "gb-project-target.h"
+#include "gb-project-target-c.h"
 
 static void
 open_cb (GObject      *object,
          GAsyncResult *result,
          gpointer      user_data)
 {
+   GbTreeNode *root;
    GbProject *project;
+   GbTree *tree = user_data;
    GError *error = NULL;
 
    project = gb_project_format_open_finish(GB_PROJECT_FORMAT(object),
@@ -19,7 +29,17 @@ open_cb (GObject      *object,
    if (!project) {
       g_warning("%s\n", error->message);
       g_error_free(error);
+      return;
    }
+
+   root = g_object_new(GB_TYPE_TREE_NODE_PROJECT,
+                       "project", project,
+                       NULL);
+
+   g_object_set(tree, "root", root, NULL);
+
+   g_object_unref(project);
+   g_object_unref(root);
 }
 
 gint
@@ -28,33 +48,23 @@ main (gint   argc,
 {
    GbProjectFormat *format;
    GInputStream *stream;
-   GbTreeNode *root;
    GtkWindow *window;
    GtkWidget *scroller;
    GtkWidget *tree;
-   GbProject *project;
    GFile *file;
 
    gtk_init(&argc, &argv);
 
+   g_type_ensure(GB_TYPE_PROJECT);
+   g_type_ensure(GB_TYPE_PROJECT_ITEM);
+   g_type_ensure(GB_TYPE_PROJECT_FILE);
+   g_type_ensure(GB_TYPE_PROJECT_GROUP);
+   g_type_ensure(GB_TYPE_PROJECT_TARGET);
+   g_type_ensure(GB_TYPE_PROJECT_TARGET_C);
+
    g_resources_register(gb_application_get_resource());
 
-   file = g_file_new_for_path(".testproject/.gbproject");
-   stream = G_INPUT_STREAM(g_file_read(file, NULL, NULL));
-   format = gb_project_format_new();
-   gb_project_format_open_async(format,
-                                ".testproject",
-                                stream,
-                                NULL,
-                                open_cb,
-                                NULL);
-   g_object_unref(format);
-   g_object_unref(file);
-   g_object_unref(stream);
-
-   project = g_object_new(GB_TYPE_PROJECT,
-                          "name", "Builder",
-                          NULL);
+   gb_icon_theme_init();
 
    window = g_object_new(GTK_TYPE_WINDOW,
                          "title", "Browser",
@@ -67,16 +77,26 @@ main (gint   argc,
                            NULL);
    gtk_container_add(GTK_CONTAINER(window), scroller);
 
-   root = g_object_new(GB_TYPE_TREE_NODE,
-                       "item", project,
-                       "text", "Test",
-                       NULL);
-
    tree = g_object_new(GB_TYPE_TREE,
-                       "root", root,
                        "visible", TRUE,
                        NULL);
    gtk_container_add(GTK_CONTAINER(scroller), tree);
+
+   gb_tree_add_builder(GB_TREE(tree),
+                       gb_tree_builder_project_new());
+
+   file = g_file_new_for_path(".testproject/.gbproject");
+   stream = G_INPUT_STREAM(g_file_read(file, NULL, NULL));
+   format = gb_project_format_new();
+   gb_project_format_open_async(format,
+                                ".testproject",
+                                stream,
+                                NULL,
+                                open_cb,
+                                tree);
+   g_object_unref(format);
+   g_object_unref(file);
+   g_object_unref(stream);
 
    g_signal_connect(window, "delete-event", gtk_main_quit, NULL);
    gtk_window_present(window);
