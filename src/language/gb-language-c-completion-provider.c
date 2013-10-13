bc13e34f70d1b3a13c961f9366e2b3ecdc2cfb08
Christian Hergert <christian@hergert.me>
2013-10-13 09:49:04 -0400

    c-completion: dont show keywords unless we have a current word.

--- src/language/gb-language-c-completion-provider.c
+++ src/language/gb-language-c-completion-provider.c
@@ -6,7 +6,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -216,13 +216,15 @@ provider_populate (GtkSourceCompletionProvider *provider,
    gtk_source_completion_context_get_iter(context, &iter);
    word = get_word(provider, &iter);
 
-   trie_traverse(priv->trie,
-                 word,
-                 G_PRE_ORDER,
-                 G_TRAVERSE_LEAVES,
-                 -1,
-                 traverse_cb,
-                 &list);
+   if (word && *word) {
+      trie_traverse(priv->trie,
+                    word,
+                    G_PRE_ORDER,
+                    G_TRAVERSE_LEAVES,
+                    -1,
+                    traverse_cb,
+                    &list);
+   }
 
    gtk_source_completion_context_add_proposals(context, provider, list, TRUE);
 
