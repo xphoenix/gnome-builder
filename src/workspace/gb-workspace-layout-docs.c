fb50d48fe42a5892baf8dbafb776857ed96473c4
Christian Hergert <christian@hergert.me>
2013-10-14 15:32:52 -0400

    docs: use devhelp layout for docs for now.
    There will be some work to do, but it will probably just happen in
    upstream devhelp. So lets just add this for now.

--- src/workspace/gb-workspace-layout-docs.c
+++ src/workspace/gb-workspace-layout-docs.c
@@ -6,7 +6,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <devhelp/devhelp.h>
 #include <glib/gi18n.h>
 #include <webkit2/webkit2.h>
 
@@ -27,7 +28,11 @@ G_DEFINE_TYPE(GbWorkspaceLayoutDocs,
 
 struct _GbWorkspaceLayoutDocsPrivate
 {
-   GtkWidget *view;
+   DhBookManager *books;
+   GtkWidget *hpaned;
+   GtkWidget *sidebar;
+   GtkWidget *scroller;
+   GtkWidget *webview;
 };
 
 static void
@@ -49,142 +54,35 @@ gb_workspace_layout_docs_class_init (GbWorkspaceLayoutDocsClass *klass)
 static void
 gb_workspace_layout_docs_init (GbWorkspaceLayoutDocs *docs)
 {
-   GtkTreeViewColumn *column;
-   GtkCellRenderer *cell;
-   GtkListStore *model;
-   GtkTreeIter iter;
-   GtkWidget *scroller;
-   GtkWidget *paned;
-   GtkWidget *vpaned;
-   GtkWidget *treeview;
-   GdkPixbuf *pixbuf;
-   GError *error = NULL;
-
    docs->priv = G_TYPE_INSTANCE_GET_PRIVATE(docs,
                                             GB_TYPE_WORKSPACE_LAYOUT_DOCS,
                                             GbWorkspaceLayoutDocsPrivate);
 
-   paned = g_object_new(GTK_TYPE_PANED,
-                        "expand", TRUE,
-                        "orientation", GTK_ORIENTATION_HORIZONTAL,
-                        "position", 275,
-                        "visible", TRUE,
-                        NULL);
-   gtk_container_add(GTK_CONTAINER(docs), paned);
+   docs->priv->books = dh_book_manager_new();
+   dh_book_manager_populate(docs->priv->books);
 
-   vpaned = g_object_new(GTK_TYPE_PANED,
-                         "expand", TRUE,
-                         "orientation", GTK_ORIENTATION_VERTICAL,
-                         "position", 400,
-                         "visible", TRUE,
-                         NULL);
-   gtk_container_add(GTK_CONTAINER(paned), vpaned);
+   docs->priv->hpaned = g_object_new(GTK_TYPE_PANED,
+                                     "expand", TRUE,
+                                     "orientation", GTK_ORIENTATION_HORIZONTAL,
+                                     "position", 325,
+                                     "visible", TRUE,
+                                     NULL);
+   gtk_container_add(GTK_CONTAINER(docs), docs->priv->hpaned);
 
-   scroller = g_object_new(GTK_TYPE_SCROLLED_WINDOW,
-                           "visible", TRUE,
-                           NULL);
-   gtk_container_add(GTK_CONTAINER(vpaned), scroller);
+   docs->priv->sidebar = dh_sidebar_new(docs->priv->books);
+   gtk_widget_set_margin_top(docs->priv->sidebar, 6);
+   gtk_container_add(GTK_CONTAINER(docs->priv->hpaned),
+                     docs->priv->sidebar);
+   gtk_widget_show(docs->priv->sidebar);
 
-   model = gtk_list_store_new(2, GDK_TYPE_PIXBUF, G_TYPE_STRING);
+   docs->priv->scroller = gtk_scrolled_window_new(NULL, NULL);
+   gtk_container_add(GTK_CONTAINER(docs->priv->hpaned),
+                     docs->priv->scroller);
+   gtk_widget_show(docs->priv->scroller);
 
-   gtk_list_store_append(model, &iter);
-   gtk_list_store_set(model, &iter, 0, NULL, 1, "GObject", -1);
-
-   treeview = g_object_new(GTK_TYPE_TREE_VIEW,
-                           "model", model,
-                           "headers-visible", FALSE,
-                           "visible", TRUE,
-                           NULL);
-   gtk_container_add(GTK_CONTAINER(scroller), treeview);
-
-   column = g_object_new(GTK_TYPE_TREE_VIEW_COLUMN,
-                         NULL);
-   gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), column);
-
-   cell = g_object_new(GTK_TYPE_CELL_RENDERER_PIXBUF,
-                       "width", 16,
-                       NULL);
-   gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(column), cell, FALSE);
-   gtk_cell_layout_add_attribute(GTK_CELL_LAYOUT(column), cell, "pixbuf", 0);
-
-   cell = g_object_new(GTK_TYPE_CELL_RENDERER_TEXT,
-                       NULL);
-   gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(column), cell, TRUE);
-   gtk_cell_layout_add_attribute(GTK_CELL_LAYOUT(column), cell, "text", 1);
-
-   scroller = g_object_new(GTK_TYPE_SCROLLED_WINDOW,
-                           "visible", TRUE,
-                           NULL);
-   gtk_container_add(GTK_CONTAINER(vpaned), scroller);
-
-   model = gtk_list_store_new(2, GDK_TYPE_PIXBUF, G_TYPE_STRING);
-
-   /*
-    * TODO: an icon abstraction for getting pixbufs with overlays.
-    */
-
-   pixbuf = gdk_pixbuf_new_from_resource("/org/gnome/Builder/data/icons/class-16x.png", &error);
-   if (!pixbuf) {
-      g_error("%s", error->message);
-   }
-
-   gtk_list_store_append(model, &iter);
-   gtk_list_store_set(model, &iter, 0, pixbuf, 1, "GObject", -1);
-   gtk_list_store_append(model, &iter);
-   gtk_list_store_set(model, &iter, 0, pixbuf, 1, "GInitiallyUnowned", -1);
-
-   pixbuf = gdk_pixbuf_new_from_resource("/org/gnome/Builder/data/icons/enum-16x.png", &error);
-   if (!pixbuf) {
-      g_error("%s", error->message);
-   }
-
-   gtk_list_store_append(model, &iter);
-   gtk_list_store_set(model, &iter, 0, pixbuf, 1, "GFileTest", -1);
-
-   treeview = g_object_new(GTK_TYPE_TREE_VIEW,
-                           "model", model,
-                           "headers-visible", FALSE,
-                           "visible", TRUE,
-                           NULL);
-   gtk_container_add(GTK_CONTAINER(scroller), treeview);
-
-   column = g_object_new(GTK_TYPE_TREE_VIEW_COLUMN,
-                         NULL);
-   gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), column);
-
-   cell = g_object_new(GTK_TYPE_CELL_RENDERER_PIXBUF,
-                       NULL);
-   gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(column), cell, FALSE);
-   gtk_cell_layout_add_attribute(GTK_CELL_LAYOUT(column), cell, "pixbuf", 0);
-
-   cell = g_object_new(GTK_TYPE_CELL_RENDERER_TEXT,
-                       NULL);
-   gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(column), cell, TRUE);
-   gtk_cell_layout_add_attribute(GTK_CELL_LAYOUT(column), cell, "text", 1);
-
-   scroller = g_object_new(GTK_TYPE_SCROLLED_WINDOW,
-                           "expand", TRUE,
-                           "visible", TRUE,
-                           NULL);
-   gtk_container_add(GTK_CONTAINER(paned), scroller);
-
-   docs->priv->view = g_object_new(WEBKIT_TYPE_WEB_VIEW,
-                                   "visible", TRUE,
-                                   NULL);
-   gtk_container_add(GTK_CONTAINER(scroller), docs->priv->view);
-
-#if 0
-   {
-      gchar *data;
-      gsize len;
-
-      /*
-       * XXX: Temporary hack to test out ideas for docs.
-       */
-      if (g_file_get_contents("data/docs.html", &data, &len, NULL)) {
-         webkit_web_view_load_html(WEBKIT_WEB_VIEW(docs->priv->view),
-                                   data, NULL);
-      }
-   }
-#endif
+   docs->priv->webview = g_object_new(WEBKIT_TYPE_WEB_VIEW,
+                                      "visible", TRUE,
+                                      NULL);
+   gtk_container_add(GTK_CONTAINER(docs->priv->scroller),
+                     docs->priv->webview);
 }
