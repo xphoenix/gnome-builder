528ffe9467993339b4c75dc42fef2f20cb605a94
Philipp Andronov <filipp.andronov@gmail.com>
2016-10-15 22:29:12 +0300

    Cmake build plugin - initial import

---
+++ plugins/cmake/ide-cmake-build-system.c
@@ -0,0 +1,13 @@
+#define G_LOG_DOMAIN "ide-cmake-build-system"
+
+#include "config.h"
+
+#include <egg-counter.h>
+#include <egg-task-cache.h>
+#include <glib/gi18n.h>
+#include <gio/gio.h>
+#include <gtksourceview/gtksource.h>
+#include <ide.h>
+#include <ide-internal.h>
+
+#include "ide-cmake-build-system.h"
---
+++ plugins/cmake/ide-cmake-build-system.h
@@ -0,0 +1,36 @@
+/* ide-cmake-build-system.h
+ *
+ * Copyright (C) 2016 Philipp Andronov <filipp.andronov@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef IDE_CMAKE_BUILD_SYSTEM_H
+#define IDE_CMAKE_BUILD_SYSTEM_H
+
+#include <ide.h>
+
+/*
+G_BEGIN_DECLS
+
+#define IDE_TYPE_AUTOTOOLS_BUILD_SYSTEM (ide_autotools_build_system_get_type())
+
+G_DECLARE_FINAL_TYPE (IdeAutotoolsBuildSystem, ide_autotools_build_system, IDE, AUTOTOOLS_BUILD_SYSTEM, IdeObject)
+
+const gchar *ide_autotools_build_system_get_tarball_name (IdeAutotoolsBuildSystem *self);
+
+G_END_DECLS
+*/
+
+#endif /* IDE_CMAKE_BUILD_SYSTEM_H */
